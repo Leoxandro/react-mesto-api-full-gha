2080c1120811852ac7c3a7a35cabeab96baf6cf4
Alexey Karpov <leoxandro@yandex.ru>
2024-01-20 20:20:43 +0300

    if changed

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('./middleware/cors');
+const cors = require('cors');
 const usersRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
@@ -13,15 +13,24 @@ const { errorHandler } = require('./middleware/error-handler');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 const { NotFoundError } = require('./utils/errors');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log(`Error during MongoDB connection: ${error}`));
 
 const app = express();
-
-app.use(cors);
 
 app.use(express.json());
 
 app.use(requestLogger);
+
+const corsOptions = {
+  origin: process.env.CORS_WHITELIST.split(','),
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -44,7 +53,7 @@ app.use('*', (res, req, next) => {
 app.use(errors());
 app.use(errorHandler);
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}`);
