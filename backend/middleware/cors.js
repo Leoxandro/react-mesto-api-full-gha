7e9f90c5a4b98247edfe6f1d29b5718a2afa2332
Alexey Karpov <leoxandro@yandex.ru>
2024-01-20 19:32:39 +0300

    if changed

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -2,8 +2,6 @@ const allowedCors = [
   'http://localhost:3000',
   'api.akarpov.students.nomoredomainsmonster.ru',
   'akarpov.students.nomoredomainsmonster.ru',
-  'http://akarpov.students.nomoredomainsmonster.ru/',
-  'http://api.akarpov.students.nomoredomainsmonster.ru/',
 ];
 
 module.exports = (req, res, next) => {
@@ -19,7 +17,8 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+
+    return res.status(200).send();
   }
 
   return next();
