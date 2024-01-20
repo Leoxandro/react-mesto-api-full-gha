35590c9c023a997ff000ea68548f519fe531e3a4
Alexey Karpov <leoxandro@yandex.ru>
2024-01-20 17:47:13 +0300

    if changed

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'api.leoxandro.students.nomoredomainsmonster.ru';
+export const BASE_URL = 'leoxandro.students.nomoredomainsmonster.ru';
 
 const handleResponse = (res) =>
   res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`);
